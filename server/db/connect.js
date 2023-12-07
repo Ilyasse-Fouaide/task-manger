ebbc3f8b68e44bf36ef72d240abc0599361cde3b
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-07 17:57:36 +0100

    connect to the mongo database

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,1 +1,16 @@
-const connectionString = "";
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://127.0.0.1:27017/TASK-MANAGER';
+
+function connect() {
+  mongoose.connect(connectionString)
+    .then((data) => {
+      console.log("connected...")
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+module.exports = connect
