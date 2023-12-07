5e05c2d13956faefdff62ca1f5253583fc0b64d5
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-07 15:45:20 +0100

    Add route "/" to return a JSON message

--- server/main.js
+++ server/main.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.route("/").get((req, res) => {
+  res.status(200).json({ message: "Task Manager app" })
+})
+
 const port = 3000;
 
 app.listen(port, () => console.log("listening..."));
