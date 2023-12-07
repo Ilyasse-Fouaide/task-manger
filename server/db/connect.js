d41cb93bc39cb077f11905269dddd8adb82c08e7
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-07 18:10:24 +0100

    refactoring: Changed the connect function to use async/await syntax.

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 const connectionString = 'mongodb://127.0.0.1:27017/TASK-MANAGER';
 
-function connect() {
-  mongoose.connect(connectionString)
-    .then(() => {
-      console.log("connected...")
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+async function connect() {
+  try {
+    await mongoose.connect(connectionString)
+    console.log("connected");
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = connect
