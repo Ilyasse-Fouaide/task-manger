43b0547b7434c392381f3947bb649661998e2aed
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-07 19:30:55 +0100

    Add required field validation for task name

--- server/models/tasks.model.js
+++ server/models/tasks.model.js
@@ -1,7 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const taskSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, "the name is required"]  // custom message
+  },
   completed: Boolean,
 });
 
