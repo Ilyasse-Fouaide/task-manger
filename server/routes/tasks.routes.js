8305f579eb1d64bb904e6770fdf51a5ad1cb9dc2
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-07 16:15:38 +0100

    Added POST route for tasks to use store controller.

--- server/routes/tasks.routes.js
+++ server/routes/tasks.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const tasks = require("../controller/tasks.controller");
 
-router.route("/").get(tasks.index);
+router.route("/")
+  .get(tasks.index)
+  .post(tasks.store);
 
 module.exports = router;
