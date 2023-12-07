17e1cf3cd965bad45dcba2c84c15a8efd2d2410e
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-07 16:14:41 +0100

    Add create task controller

--- server/controller/tasks.controller.js
+++ server/controller/tasks.controller.js
@@ -1,3 +1,7 @@
 module.exports.index = (req, res) => {
   res.status(200).json({ message: "Get All tasks" })
 }
+
+module.exports.store = (req, res) => {
+  res.status(200).json({ message: "Create task." });
+}
