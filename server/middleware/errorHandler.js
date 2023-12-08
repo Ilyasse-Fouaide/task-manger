8bcbb1698e5d9890d6dde59bf1f9454f137122e2
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-08 10:21:18 +0100

    check if err is instance of CustomError class

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,11 +1,22 @@
+const { CustomError } = require("../errors/customError");
+
 const errorHandler = (err, req, res, next) => {
-  res.status(err.status).json({
+  if (err instanceof CustomError) {
+    res.status(err.status).json({
+      success: false,
+      error: {
+        status: err.status,
+        message: err.message
+      }
+    });
+  }
+  res.status(500).json({
     success: false,
     error: {
-      status: err.status,
-      message: err.message
+      status: 500,
+      message: "Something Broke!"
     }
-  });
+  })
 }
 
 module.exports = errorHandler;
