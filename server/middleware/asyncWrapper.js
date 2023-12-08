500f613df940437459111165a97ce3001b6fdf38
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-12-08 14:30:34 +0100

    Update error handling middleware to use custom error class and improve error messages.

--- server/middleware/asyncWrapper.js
+++ server/middleware/asyncWrapper.js
@@ -1,9 +1,11 @@
+const { customError } = require("../errors/customError");
+
 const asyncWrapper = (cb) => {
   return async (req, res, next) => {
     try {
       await cb(req, res, next);
     } catch (error) {
-      next(error);
+      next(customError("Something Broke!", 500));
     }
   }
 }
--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,22 +1,13 @@
 const { CustomError } = require("../errors/customError");
 
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof CustomError) {
-    return res.status(err.status).json({
-      success: false,
-      error: {
-        status: err.status,
-        message: err.message
-      }
-    });
-  }
-  res.status(500).json({
+  return res.status(err.status).json({
     success: false,
     error: {
-      status: 500,
-      message: "Something Broke!"
+      status: err.status,
+      message: err.message
     }
-  })
+  });
 }
 
 module.exports = errorHandler;
